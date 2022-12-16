04de318203699d1b57d3bb247a1d509ed1036100
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-12-16 15:49:32 +0100

    Inserito un input form per la ricerca delle canzoni tramite il titolo

--- js/main.js
+++ js/main.js
@@ -4,15 +4,36 @@ createApp({
     data() {
         return {
             vinylLists: [],
+            songTitle: '',
+        }
+    },
+    methods: {
+        searchInAPI(){
+            axios.get('http://localhost/php-dischi-json/api.php', {
+                params: {
+                }
+            })
+            .then(res => {
+                this.vinylLists = res.data;
+            });
+        },
+        searchByName(){
+            this.vinylLists = [];
+            axios.get('http://localhost/php-dischi-json/api.php', {
+                params: {
+                    song_name: this.songTitle,
+                }
+            })
+            .then(res => {
+                res.data.forEach(element => {
+                    if(element.title.toLowerCase().includes(this.songTitle)){
+                        this.vinylLists.push(element);
+                    }
+                });
+            });
         }
     },
     created(){
-        axios.get('http://localhost/php-dischi-json/api.php', {
-            params: {
-            }
-        })
-        .then(res => {
-            this.vinylLists = res.data;
-        });
+        this.searchInAPI();
     }
 }).mount('#app')
